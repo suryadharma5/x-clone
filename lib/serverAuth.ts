4d695b22a8c4c94a9336ed1cc108459050527b51
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-12-22 17:52:46 +0700

    fixed: server auth using get server session

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,11 +1,11 @@
-import { NextApiRequest } from "next";
-import { getSession } from "next-auth/react";
-import prisma from "@/lib/prismadb"
+import { NextApiRequest, NextApiResponse } from "next";
 
-const serverAuth = async (req: NextApiRequest) => {
-    const session = await getSession({
-        req: req
-    })
+import prisma from "@/lib/prismadb"
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getServerSession(req, res, authOptions)
 
     if (!session?.user?.email) {
         throw new Error("Not signed in")
