9855339db43bcddf2a73364eca4c8a6c6ad55dc0
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-12-23 00:00:24 +0700

    feat: tweet functionallity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
+import Form from '@/components/Form'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <Header label="Home" />
+    <>
+      <Header label="Home" />
+      <Form placeHolder="What's happening ?" />
+    </>
+
   )
 }
