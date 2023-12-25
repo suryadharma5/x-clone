83596f5a07bb3a388ac04582d4457ada6382b12b
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-12-25 10:26:04 +0700

    feat: post detail ui

---
+++ pages/posts/[postId].tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import usePost from '@/hooks/usePost'
+
+import { useRouter } from 'next/router'
+import { ClipLoader } from 'react-spinners'
+
+import Header from '@/components/Header'
+import PostItem from '@/components/posts/PostItem'
+import Form from '@/components/Form'
+
+const PostView = () => {
+    const router = useRouter()
+    const { postId } = router.query
+
+    const { data: fetchedPost, isLoading } = usePost(postId as string)
+
+    if (isLoading || !fetchedPost) {
+        return (
+            <div className='flex justify-center items-center h-full'>
+                <ClipLoader color='lightblue' size={80} />
+            </div>
+        )
+    }
+
+    console.log(fetchedPost)
+
+    return (
+        <>
+            <Header label='Tweet' showBackArrow />
+            <PostItem data={fetchedPost} />
+            <Form
+                postId={postId as string}
+                isComment
+                placeHolder='Tweet your reply'
+            />
+        </>
+    )
+}
+
+export default PostView
