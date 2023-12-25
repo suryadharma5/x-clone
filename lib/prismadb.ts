0ccb6a258cf5eb83863e9ffa8221b02485e896ef
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-12-25 21:16:53 +0700

    fix: CORS issue

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -8,6 +8,6 @@ const client = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
 
-if (process.env.NODE_ENV === 'production') globalThis.prisma = new PrismaClient()
+// if (process.env.NODE_ENV === 'production') globalThis.prisma = new PrismaClient()
 
 export default client
