f59aed40c867450505bbedd8c00135c14d262796
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-12-25 16:40:09 +0700

    fix: CORS issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://x-clone-not-real.vercel.app/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
