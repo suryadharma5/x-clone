4f491c39c20cab06b79a696d54fa649b45b862dc
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-12-27 23:49:00 +0800

    chore: sentences in home page

--- components/layout/Followbar.tsx
+++ components/layout/Followbar.tsx
@@ -8,7 +8,7 @@ const FollowBar = () => {
     return (
         <div className='px-6 py-4 hidden lg:block col-span-1'>
             <div className='bg-neutral-800 rounded-xl p-4'>
-                <h2 className='text-white text-xl font-semibold'>Who to follow</h2>
+                <h2 className='text-white text-xl font-semibold'>See all users</h2>
                 <div className='flex flex-col gap-6 mt-4'>
                     {users.map((user: Record<string, any>) => (
                         <div key={user.id} className='flex flex-row gap-4'>
